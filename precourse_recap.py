7f94f0b7437cbee0f8cab6f7d89116aff12d53d6
Chris <chrisphilholt@gmail.com>
2022-06-20 20:13:31 +0100

    the initial commit

---
+++ precourse_recap.py
@@ -0,0 +1,19 @@
+name = 'chris'
+age = 25
+a_float = 3.14159
+
+
+person = {
+    'name': 'chris',
+    'age': 25,
+    'height': '5,11',
+    'hobbies': 'BMX and manipulating the DOM'
+}
+
+desired_age = age - 7
+
+
+print(desired_age)
+print(type(name))
+print(person['name'])
+print(name.upper())
